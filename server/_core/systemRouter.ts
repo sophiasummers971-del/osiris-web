a5e94d6dcaa1b1814d5a1efb5946f281835a3c29
Sophia Jade Summer's <244750225+sophiasummers971-del@users.noreply.github.com>
2026-08-02 17:23:03 +0100

    fix Node ESM import resolution

--- server/_core/systemRouter.ts
+++ server/_core/systemRouter.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
-import { notifyOwner } from "./notification";
-import { adminProcedure, protectedProcedure, publicProcedure, router } from "./trpc";
-import { ENV } from "./env";
+import { notifyOwner } from "./notification.js";
+import { adminProcedure, protectedProcedure, publicProcedure, router } from "./trpc.js";
+import { ENV } from "./env.js";
 
 export const systemRouter = router({
   health: publicProcedure
