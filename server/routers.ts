0256acf604a4ee1e2da1eaa0e306f3ab814e9db7
Sophia Jade Summer's <244750225+sophiasummers971-del@users.noreply.github.com>
2026-08-02 11:26:19 +0100

    Build OSIRIS security foundation: server/routers.ts

--- server/routers.ts
+++ server/routers.ts
@@ -3,10 +3,11 @@
 import { getSessionCookieOptions } from "./_core/cookies";
 import { systemRouter } from "./_core/systemRouter";
 import { publicProcedure, router } from "./_core/trpc";
-import { COOKIE_NAME } from "./_core/cookies";
+import { COOKIE_NAME } from "@shared/const";
 import { getSupporterTiers, getExclusiveContent, getSupporterStats } from "./supporters";
 import { notificationRouter } from "./notifications";
 import { createCheckoutSession, getUserOrders, getUserSubscription, getProducts } from "./stripe";
+import { casesRouter } from "./cases";
 
 export const appRouter = router({
   // if you need to use socket.io, read and register route in server/_core/index.ts, all api should start with '/api/' so that the gateway can route correctly
@@ -37,6 +38,7 @@ export const appRouter = router({
   }),
 
   notifications: notificationRouter,
+  cases: casesRouter,
 
   stripe: router({
     createCheckoutSession,
